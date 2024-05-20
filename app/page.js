7844bd17ac17cfbb0d6bde128266a2b7b04df546
iRatkin <ADH.2234@gmail.com>
2024-05-21 02:12:19 +0300

    Init

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome SharDev NextJS course</h1>
+    </main>
+  );
+}
